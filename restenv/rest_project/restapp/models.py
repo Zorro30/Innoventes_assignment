87b88a3720aa0240ff0f208fdd6511c4cfbbfce7
Zorro30 <gbp301195@gmail.com>
2018-12-22 17:21:17 +0530

    Code

--- restenv/rest_project/restapp/models.py
+++ restenv/rest_project/restapp/models.py
@@ -4,4 +4,6 @@ class Employee_details(models.Model):
     First_name = models.CharField(max_length = 200)
     Last_name = models.CharField(max_length = 200)
     Date_of_Birth = models.CharField(max_length = 200)
-    Address = models.CharField(max_length = 200)
+    Address_Present = models.CharField(max_length = 200, blank = True,)
+    Address_Permanent = models.CharField(max_length = 200, blank = True,)
+    Address_Office = models.CharField(max_length = 200, blank = True,)
--- restenv/rest_project/restapp/serializers.py
+++ restenv/rest_project/restapp/serializers.py
@@ -2,6 +2,8 @@ from .models import Employee_details
 from rest_framework import  serializers
 
 class EmployeeSerializers(serializers.ModelSerializer):
+
     class Meta:
         model = Employee_details
-        fields = ('id','First_name','Last_name','Date_of_Birth','Address')
+        fields = ('id','First_name','Last_name','Date_of_Birth','Address_Present','Address_Permanent','Address_Office')
+        # fields = '__all__'
